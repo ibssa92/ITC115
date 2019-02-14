10332f7a8c843b18e0bf3e3cc883790a1083162b
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-02-13 20:05:21 -0800

    Update randomWalk.java

--- randomWalk.java
+++ randomWalk.java
@@ -3,45 +3,40 @@ import java.util.Random;
 public class randomWalk {
 
 	public static void main(String[] args) {
+		randomWalk();
+
+	}
+
+	public static void randomWalk() {
+		
+		
 		// initialize the position and the max position
 		int position = 0;
 		int max = 0;
-		while (position <= 3 && position >= -3) {
-			for (int i = 1; i <= 100; i++) {// position starts as 0. increases or decreases based off randomWalk (+1 or
-											// -1)
-				position += randomWalk();
+		
+		
 
-				System.out.println("position = " + position); // print each position in the loop
+		while (position != 3 && position != -3) {//keep the while loop going until it hits 3 or -3
+			Random rand = new Random();
+			int randomstep = rand.nextInt(2);
+
+			if (randomstep == 0) { // if random number is 0 increment +1. Else it would be 1 which decreases -1
+				position++;
+				if (position > max) { // if the current position is greater than the current max, the max updates to that
+					max = position;
+				}
+			} else {
+
+				position--;
+				
 			}
-		}
-		// if the highest position is a negative integer, print max position as 0.
-		// Otherwise, the max is the highest position
-		if (position < 0) {
-			max = 0;
-		}
-		max = position;
+			//print current position
+			System.out.println("Position = " + position);
 
-		System.out.println("max = " + max); // print the max position
+		}
+		
+		
+		
+		System.out.println("Max Position = " + max);
 	}
-	
-	
-	
-
-	public static int randomWalk() {
-
-		// create instance of Random Class
-		Random rand = new Random();
-
-		// generate random integers 0 or 1; 1 would be +1 step forward and 0 would be -1
-		// step backwards
-		int randomstep = rand.nextInt(2);
-
-		if (randomstep == 0) {
-			return -1;
-		}
-
-		return 1;
-
-	}
-
 }
