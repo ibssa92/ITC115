5bde2a400c65c670c415b8e7db9a1acb786ac6cb
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-02-25 16:43:23 -0800

    Add files via upload

---
+++ Assignment8.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Assignment8 {
+
+	public static void main(String[] args) {
+		int[] list1 = { 1, 2, 3, 4, 5, 6 };
+		int[] list2 = { 2, 1, 4, 3, 6, 5 };
+
+		if (isSorted(list1)) {
+			System.out.println("list1 is ascending");
+		} else {
+			System.out.println("list1 IS NOT ascending");
+		}
+
+		System.out.println("");
+
+		if (isSorted(list2)) {
+			System.out.println("list2 is ascending");
+		} else {
+			System.out.println("list2 IS NOT ascending");
+		}
+
+	}
+
+	public static boolean isSorted(int[] list) {
+
+		String s = Arrays.toString(list);
+		System.out.println(s);
+
+		Arrays.sort(list);
+		String z = Arrays.toString(list);
+		System.out.println(z);
+
+		if (s != z) {
+			return false;
+		} 
+		
+		else {
+
+			return true;
+		}
+	}
+
+}
